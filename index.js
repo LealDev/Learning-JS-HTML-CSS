aa893f1f12650bea8249e7ddc67cba3a0c5e468d
LealDev <lealmaster0111@gmail.com>
2022-08-17 19:27:59 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * aqui se inicia minha jornada para aprender JavaScript
+ * eu sou emanuel, e serei um ótimo programador.
+ */
+var olaMundo = "Hello World"
+console.log('Olá mundo');
+console.log(olaMundo);
+
+let a = 10;
+const b = "10";
+
+console.log(a === b || typeof olaMundo == b);
+
+let cor = "amarelo";
+
+if (cor === "vermelho") {
+    console.log("siga");
+}else if(cor === "amarelo"){
+    console.log("atenção");
+}
+else{
+    console.log("pare");
+}
+
---
+++ switch.js
@@ -0,0 +1,15 @@
+let cor = "azul";
+
+switch (cor) {
+    case "verde":
+        console.log("Siga")
+        break;
+    case "amarelo":
+        console.log("Atenção")
+        break;
+    case "Vermelho":
+        console.log("Pare")
+        break;
+    default: 
+        console.log("Não sei")
+    }
