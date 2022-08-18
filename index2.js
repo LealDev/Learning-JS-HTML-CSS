38af4164a803bfeb1e7255f48e2b82d3a9120cd4
LealDev <lealmaster0111@gmail.com>
2022-08-17 21:49:15 -0300

    forEach

--- index2.js
+++ index2.js
@@ -24,3 +24,17 @@ let agora = new Date();
 console.log(agora.getFullYear());
 
 console.log(agora.toLocaleDateString("pt-BR"));
+
+// arrays
+
+let carros = ["palio 98", "toro", "uno", 10, true, new Date()];
+console.log(carros[5].getFullYear());
+
+carros.forEach(function(value, index){
+    console.log(index, value);
+})
+
+let biblioteca = ["the beast and the bealty", 10,"the lord of the ring",5,"the thief", 2, calc(10,5,"+")];
+biblioteca.forEach(function(value,index){
+    console.log(index,value)
+})
